e0e3bb446f0133092a309a95a84f5e3b7b2522f9
HarishSurya <38115933+HarishSurya@users.noreply.github.com>
2021-08-19 01:00:31 +0530

    Update index.js

--- src/Sections/Contact/index.js
+++ src/Sections/Contact/index.js
@@ -146,11 +146,11 @@ const Contact = () => {
       </Icons>
       <Form>
         <Row>
-          <input name="name" type="text" placeholder="your name" />
+          <input name="name" type="text" placeholder="Enter your name" />
           <input
             name="email"
             type="email"
-            placeholder="enter working email id"
+            placeholder="Enter your Email id"
           />
         </Row>
         <textarea
@@ -158,7 +158,7 @@ const Contact = () => {
           id=""
           cols="30"
           rows="2"
-          placeholder="your message"
+          placeholder="Enter your message"
         ></textarea>
         <div style={{ margin: "0 auto" }}>
           <button
